7585afc3d07ed798f3ae1ce9b90bb1ac72f9cf70
olanetsoft <heedris2olubisi@gmail.com>
2020-04-19 11:03:51 +0100

    UPDATED LOGO

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,13 +19,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Tutor-X`,
+        short_name: `Tutor-X`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/tutorLogo.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-sass`,
