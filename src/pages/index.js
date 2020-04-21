9d91f14478a8ef5e02e93658b4a634eb7d25eed1
olanetsoft <heedris2olubisi@gmail.com>
2020-04-21 11:16:05 +0100

    MARGIN FIXED TO CONTACT SECTION

--- src/pages/index.js
+++ src/pages/index.js
@@ -396,11 +396,11 @@ export default class IndexPage extends React.Component {
               </div>
             </div>
             <div className="row">
-              <div className="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+              <div className="col-sm-4 ml-auto text-center mb-5 mb-lg-0">
                 <i className="fas fa-phone fa-3x mb-3 text-muted"></i>
                 <div>+2348143166543</div>
               </div>
-              <div className="col-lg-4 mr-auto text-center">
+              <div className="col-sm-4 mr-auto text-center">
                 <i className="fas fa-envelope fa-3x mb-3 text-muted"></i>
                 <a className="d-block" href="mailto: ugeorge78@gmail.com"> info.tutorX@gmail.com </a>
               </div>
