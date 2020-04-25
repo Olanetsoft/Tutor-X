c60a4354c63c3c95866af8ff80d2939931a3ea25
olanetsoft <heedris2olubisi@gmail.com>
2020-04-25 21:01:14 +0100

    INSTALLED GOOGLE ANALYTICS

--- src/components/layout.js
+++ src/components/layout.js
@@ -65,17 +65,17 @@ export default class Layout extends React.Component {
         <footer className="bg-dark p-2">
           <div class="row">
 
-            <div className="col-md-12  text-center">
+            <div className="col-md-12 text-center">
               <div className="mb-4 flex-center">
 
 
                 <a className="fb-ic" href="https://twitter.com/tutorXApp">
                   <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3">facebook </i>
-                  </a>
+                </a>
 
                 <a className="tw-ic" href="https://twitter.com/tutorXApp">
                   <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3"> twitter </i>
-                 </a>
+                </a>
 
                 {/* <a className="gplus-ic" href="https://twitter.com/tutorXApp">
                   <i className="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"> </i>
@@ -87,7 +87,7 @@ export default class Layout extends React.Component {
 
                 <a className="ins-ic" href="https://instagram.com/official_tutorX">
                   <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3"> instagram </i>
-                 </a>
+                </a>
 
                 {/* <a className="pin-ic" href="https://twitter.com/tutorXApp">
                   <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
